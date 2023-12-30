05656ee55e1d73038eb03dd4f9620b6fca3a1c2c
Deyu Tan <116015021+gushouchuanzhi1@users.noreply.github.com>
2023-12-30 23:27:28 +0800

    P1996
    P1996

---
+++ c/P1996/P1996/P1996.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n, m;//n个人，第m个人出圈
+	int t = 0;
+	scanf("%d %d", &n, &m);
+	int array[105] = { 0 };
+	for (int i = 0; i < n; i++) {//数组标记
+		for (int j = 0; j < m; j++) {
+			if (++t > n) t = 1;
+			if (array[t]) j--;
+		}
+		printf("%d ", t);
+		array[t] = 1;
+	}
+	return 0;
+}
+
+
+
+
