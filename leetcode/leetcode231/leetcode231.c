67f686de404df64255ef1517d953a9dae486f027
Deyu Tan <116015021+gushouchuanzhi1@users.noreply.github.com>
2024-06-23 11:27:58 +0800

    update 20240623 leetcode231

---
+++ leetcode/leetcode231/leetcode231.c
@@ -0,0 +1,9 @@
+bool isPowerOfTwo(int n) {
+    if(n <= 0) return false; 
+    while(n > 0){
+        if(n == 1) return true;
+        if(n % 2 == 1) return false;
+        n = n / 2;
+    }
+    return true;
+}
